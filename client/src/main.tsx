7259163698eda95e53174e2152248cf29dfb67bc
callcolor <call.color@gmail.com>
2025-07-20 09:40:59 -0500

    feat: clarity

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import Clarity from "@microsoft/clarity";
+
+Clarity.init("shwjbrge5h");
 
 createRoot(document.getElementById("root")!).render(<App />);
